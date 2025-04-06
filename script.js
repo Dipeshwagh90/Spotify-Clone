2f78b19c19784a08ce7c04d73f0e89b5dfa0fdce
jaya2703 <nskjayashreethakare273@gmail.com>
2025-04-06 21:56:08 +0530

    fifth  commit

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const playMusic = (track, pause = false) => {
     currentSong.src = "/songss/" + track
     if (!pause) {
         currentSong.play()
-        play.src = "pause.svg"
+        play.src = "img/pause.svg"
     }
     document.querySelector(".songinfo").innerHTML = decodeURI(track)
     document.querySelector(".songtime").innerHTML = "00.00 / 00.00"
@@ -52,14 +52,14 @@ async function main() {
     //show all the songs in the playlist
     let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
     for (const song of songss) {
-        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="music.svg" alt="">
+        songUL.innerHTML = songUL.innerHTML + `<li><img class="invert" src="img/music.svg" alt="">
                             <div class="info">
                                 <div>${song.replaceAll("%20", " ")}</div>
                                 <div>Jaya</div>
                             </div>
                             <div class="playnow">
                                 <span>Play now</span>
-                                <img class="invert" src="play.svg" alt="">
+                                <img class="invert" src="img/play.svg" alt="">
                             </div> </li>`;
     }
 
@@ -77,11 +77,11 @@ async function main() {
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play()
-            play.src = "pause.svg"
+            play.src = "img/pause.svg"
         }
         else {
             currentSong.pause()
-            play.src = "play.svg"
+            play.src = "img/play.svg"
         }
     })
 
